7568d62c6017ac716ed3a0ede856eb741f4b6b5e
ydtg1993 <946818508@qq.com>
2023-06-12 23:35:31 +0800

    1

--- src/pages/common/navigation/index.js
+++ src/pages/common/navigation/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import {
-    BottomFloor,
-    Bottom,
-    NavigationTab,
-    NavHome,
-    NavMenu,
-    NavSearch,
-    NavIBook,
-    NavMe,
-    NavHomeSelected, NavMenuSelected, NavSearchSelected, NavIBookSelected, NavMeSelected
-} from './style';
-import {connect} from "react-redux";
-import {Link, useLocation} from "react-router-dom";
+import { BottomFloor, Bottom, NavigationTab, NavHome, NavMenu, NavSearch, NavIBook, NavMe, NavHomeSelected, NavMenuSelected, NavSearchSelected, NavIBookSelected, NavMeSelected } from './style';
+import { connect } from "react-redux";
+import { NavLink, useLocation } from "react-router-dom";
+
+const NavLinkWithIcon = React.memo(({ to, iconSelected, icon }) => {
+    const location = useLocation();
+    const isActive = (location.pathname === to);
+
+    let selectedIcon = null;
+
+    switch (to) {
+        case "/":
+            selectedIcon = <NavHomeSelected/>;
+            break;
+        case "/menu":
+            selectedIcon = <NavMenuSelected/>;
+            break;
+        case "/search":
+            selectedIcon = <NavSearchSelected/>;
+            break;
+        case "/ibook":
+            selectedIcon = <NavIBookSelected/>;
+            break;
+        case "/me":
+            selectedIcon = <NavMeSelected/>;
+            break;
+    }
+
+    return (
+        <NavLink to={to} exact replace={isActive}>
+            <NavigationTab activeClassName={isActive ? "active" : ""}>
+                {isActive ? (selectedIcon || iconSelected) : icon}
+            </NavigationTab>
+        </NavLink>
+    );
+});
 
 class NavComponent extends React.Component {
     constructor(props) {
@@ -29,45 +52,12 @@ class NavComponent extends React.Component {
                     <NavLinkWithIcon to="/me" exact iconSelected={<NavMeSelected/>} icon={<NavMe/>}/>
                 </Bottom>
             </BottomFloor>
-        )
+        );
     }
 }
 
 const mapStateToProps = (state) => {
-    return {}
+    return {};
 };
 
 export default connect(mapStateToProps, null)(NavComponent);
-
-const NavLinkWithIcon = ({to, iconSelected, icon}) => {
-    const location = useLocation();
-    const isActive = (location.pathname === to);
-
-    let selectedIcon = null;
-
-    switch (to) {
-        case "/":
-            selectedIcon = <NavHomeSelected/>;
-            break;
-        case "/menu":
-            selectedIcon = <NavMenuSelected/>;
-            break;
-        case "/search":
-            selectedIcon = <NavSearchSelected/>;
-            break;
-        case "/rank":
-            selectedIcon = <NavIBookSelected/>;
-            break;
-        case "/me":
-            selectedIcon = <NavMeSelected/>;
-            break;
-    }
-
-    return (
-        <Link to={to} replace={isActive}>
-            <NavigationTab>
-                {isActive ? (selectedIcon || iconSelected) : icon}
-            </NavigationTab>
-        </Link>
-    );
-};
