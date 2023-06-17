0c108d2988b96d65651601f1dfcf9ffa865f0d6f
ydtg1993 <946818508@qq.com>
2023-06-17 12:55:59 +0800

    1

--- src/pages/hall/navigation/index.js
+++ src/pages/hall/navigation/index.js
@@ -14,14 +14,15 @@ class NavComponent extends React.Component {
     }
 
     render() {
+        let navStyle = {display:'flex',alignItems:'center',height:'58px'};
         return (
             <BottomFloor>
                 <Bottom>
-                    <NavLink to="/" exact><NavigationTab className={"home"} /></NavLink>
-                    <NavLink to="/menu" exact><NavigationTab className={this.props.currentPosition === 'menu' ? 'menu-in' : 'menu'} /></NavLink>
-                    <NavLink to="/search" exact><NavigationTab className={"search"} /></NavLink>
-                    <NavLink to="/ibook" exact><NavigationTab className={this.props.currentPosition === 'ibook' ? 'ibook-in' : 'ibook'} /></NavLink>
-                    <NavLink to="/me" exact><NavigationTab className={this.props.currentPosition === 'me' ? 'me-in' : 'me'} /></NavLink>
+                    <NavLink to="/" exact style={navStyle}><NavigationTab className={"home"} /></NavLink>
+                    <NavLink to="/menu" exact style={navStyle}><NavigationTab className={this.props.currentPosition === 'menu' ? 'menu-in' : 'menu'} /></NavLink>
+                    <NavLink to="/search" exact style={navStyle}><NavigationTab className={"search"} /></NavLink>
+                    <NavLink to="/ibook" exact style={navStyle}><NavigationTab className={this.props.currentPosition === 'ibook' ? 'ibook-in' : 'ibook'} /></NavLink>
+                    <NavLink to="/me" exact style={navStyle}><NavigationTab className={this.props.currentPosition === 'me' ? 'me-in' : 'me'} /></NavLink>
                 </Bottom>
             </BottomFloor>
         );
--- src/style.js
+++ src/style.js
@@ -23,58 +23,9 @@ body {
      font-family: 'MyFont';
      src: url('${MyFont}');
   }
-  
-  #VerticalScreen{
-  height: calc(var(--vh, 1vh)*100);
-  width: 100%;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  z-index: 100000;
-  display: none;
-  background: #29292a;
-  color: white;
-  text-align: center;
-  }
-
-#MongolianScreen{
-  height: 100%;
-  width: 100%;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  z-index:1999;
-  background-color:rgba(0, 0, 0, 0.5);
 }
 
-#GameMongolianScreen{
-  height: 100%;
-  width: 100%;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  z-index:1999;
-  background-color:rgba(0, 0, 0, 0.2);
-}
-
-.CloseMongolian{
-  display: none;
-}
-
-.ShowMongolian{
-  display: block;
-}
-  
-  @media screen and (max-width:319px){
-    #VerticalScreen{
-        display: block;
-    }
-  }
-  
-  @media screen and (min-width:1025px){
-    #VerticalScreen{
-        display: block;
-    }
-  }
+a {
+  -webkit-tap-highlight-color: transparent;
 }
 `;
