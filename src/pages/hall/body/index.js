ac348d838231f5dcb870628cd695732b778385cc
ydtg1993 <946818508@qq.com>
2023-06-25 21:26:37 +0800

    1

--- src/pages/hall/body/index.js
+++ src/pages/hall/body/index.js
@@ -6,7 +6,7 @@ const BodyComponent = ({ children, loadMoreData,loadMoreEnd }) => {
     const [isLoading, setIsLoading] = useState(false);
     const bodyRef = useRef(null);
 
-    useEffect(() => {
+    const scrollEvent = () => {
         const handleScroll = () => {
             const { scrollTop, clientHeight, scrollHeight } = bodyRef.current;
             const isBottom = scrollTop + clientHeight + 20 >= scrollHeight;
@@ -15,29 +15,26 @@ const BodyComponent = ({ children, loadMoreData,loadMoreEnd }) => {
 
         const bodyElement = bodyRef.current;
         bodyElement.addEventListener('scroll', handleScroll);
-
-        return () => {
-            bodyElement.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
+    };
 
     useEffect(async () => {
+        scrollEvent();
         if(loadMoreEnd === 1){
             return;
         }
         if (isBottom && !isLoading) {
             setIsLoading(true);
-            setIsBottom(false);
             await new Promise((resolve) => {
                 setTimeout(() => {
                     resolve();
-                }, 1500);
+                }, 1000);
             });
+            setIsBottom(false);
             loadMoreData().finally(async () => {
                 await new Promise((resolve) => {
                     setTimeout(() => {
                         resolve();
-                    }, 3000);
+                    }, 2000);
                 });
                 setIsLoading(false);
             });
@@ -45,10 +42,11 @@ const BodyComponent = ({ children, loadMoreData,loadMoreEnd }) => {
     }, [isBottom, loadMoreData,loadMoreEnd, isLoading]);
 
     const loadingAnimation = () => {
+        if(loadMoreEnd === 1){
+            return (<LoadingSection>-END-</LoadingSection>);
+        }
         if(isLoading){
             return (<LoadingSection><LoadingIcon /></LoadingSection>);
-        }else if(loadMoreEnd === 1){
-            return (<LoadingSection>-END-</LoadingSection>);
         }
     };
 
