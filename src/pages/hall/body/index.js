0a21ec77459ec31e771803477a08f34702920eba
ydtg1993 <946818508@qq.com>
2023-07-03 21:05:01 +0800

    1

--- src/pages/hall/body/index.js
+++ src/pages/hall/body/index.js
@@ -1,52 +1,49 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {LoadingIcon, LoadingSection, Section} from './style';
 
-const BodyComponent = ({ children, loadMoreData,loadMoreEnd }) => {
+const BodyComponent = ({children, loadMoreData, loadMoreEnd}) => {
     const [isBottom, setIsBottom] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
     const bodyRef = useRef(null);
 
-    const scrollEvent = () => {
+    useEffect(() => {
         const handleScroll = () => {
-            const { scrollTop, clientHeight, scrollHeight } = bodyRef.current;
+            const {scrollTop, clientHeight, scrollHeight} = bodyRef.current;
             const isBottom = scrollTop + clientHeight + 20 >= scrollHeight;
             setIsBottom(isBottom);
         };
 
         const bodyElement = bodyRef.current;
         bodyElement.addEventListener('scroll', handleScroll);
-    };
 
-    useEffect(async () => {
-        scrollEvent();
-        if(loadMoreEnd === 1){
+        return () => {
+            bodyElement.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (loadMoreEnd === 1) {
             return;
         }
         if (isBottom && !isLoading) {
             setIsLoading(true);
-            await new Promise((resolve) => {
-                setTimeout(() => {
-                    resolve();
-                }, 1000);
-            });
-            setIsBottom(false);
-            loadMoreData().finally(async () => {
-                await new Promise((resolve) => {
+            setTimeout(() => {
+                setIsBottom(false);
+                loadMoreData().finally(async () => {
                     setTimeout(() => {
-                        resolve();
+                        setIsLoading(false);
                     }, 2000);
                 });
-                setIsLoading(false);
-            });
+            }, 1000);
         }
-    }, [isBottom, loadMoreData,loadMoreEnd, isLoading]);
+    }, [isBottom, loadMoreData, loadMoreEnd, isLoading]);
 
     const loadingAnimation = () => {
-        if(loadMoreEnd === 1){
+        if (loadMoreEnd === 1) {
             return (<LoadingSection>-END-</LoadingSection>);
         }
-        if(isLoading){
-            return (<LoadingSection><LoadingIcon /></LoadingSection>);
+        if (isLoading) {
+            return (<LoadingSection><LoadingIcon/></LoadingSection>);
         }
     };
 
