3bb1bc7b510272173fb7ccb271a2f44161f7d8c1
ydtg1993 <946818508@qq.com>
2023-07-29 21:21:48 +0800

    1

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 
 import HallReducer from '../pages/hall/store/reducer';
 import HomeReducer from '../pages/hall/home/store/reducer';
+import MenuReducer from '../pages/hall/menu/store/reducer';
 import SearchReducer from '../pages/hall/search/store/reducer';
 import ComicReducer from '../pages/comic/store/reducer';
 import ChapterReducer from '../pages/chapter/store/reducer';
@@ -9,6 +10,7 @@ import ChapterReducer from '../pages/chapter/store/reducer';
 export default combineReducers({
     hall:HallReducer,
     home:HomeReducer,
+    menu:MenuReducer,
     search:SearchReducer,
     comic:ComicReducer,
     chapter:ChapterReducer,
