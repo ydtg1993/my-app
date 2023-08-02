4c6e864edc0eb173cec23a25f777166d776590bd
ydtg1993 <946818508@qq.com>
2023-08-02 21:39:56 +0800

    1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-export const Host = 'http://www.dmwon.co/api/';
-export const WebHost = 'http://www.dmwon.com/';
+export const Host = 'https://www.dmwon.co/api/';
+export const WebHost = 'https://www.dmwon.com/';
 ReactDOM.render(<App />, document.getElementById('root'));
