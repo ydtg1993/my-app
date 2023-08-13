0f5cbfafb036a751fd2fcc8e3954042dc738d6d3
ydtg1993 <946818508@qq.com>
2023-08-13 11:12:08 +0800

    1

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -5,7 +5,7 @@ const NoFound = () => {
     return (
         <div>
             <h1 style={{textAlign:'center'}}>404 Not Found</h1>
-            <img src={dog_funny}/>
+            <img width={'320px'} src={dog_funny}/>
             <p style={{textAlign:'center'}}>The page you are looking for does not exist.</p>
             <h3 style={{textAlign:'center'}}><span id="siteseal"><script async type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=jw1Iq785ly1PLxAenff6dLUnxzoWtocwrn6hvfVBYHyOk7nw7WzVnNDbRRO3"></script></span></h3>
         </div>
